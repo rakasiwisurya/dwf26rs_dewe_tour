8cfd4d179e08c9785fce9f8dd2f42d56d555ac2c
Rakasiwi Surya <rakasiwi.surya@gmail.com>
2021-10-23 00:40:30 +0700

    add register

--- src/components/Header.js
+++ src/components/Header.js
@@ -6,61 +6,96 @@ import BrandIcon from "assets/images/dewe-tour-icon.png";
 import Avatar from "assets/images/user.png";
 
 export default function Header() {
-  const dataUsers = JSON.parse(localStorage.getItem("deweTourUsers"));
+  const users = JSON.parse(localStorage.getItem("deweTourUsers"));
 
-  const [login, setLogin] = useState({ showLogin: false, isLogin: false });
-  const [users, setUsers] = useState(dataUsers);
-  const [form, setForm] = useState({ email: "", password: "" });
+  const [state, setState] = useState({
+    fullname: "",
+    email: "",
+    password: "",
+    phone: "",
+    address: "",
+    showRegister: false,
+    showLogin: false,
+    isLogin: false,
+  });
 
   const handleClose = () => {
-    setLogin((previousState) => {
-      return { ...previousState, showLogin: false };
-    });
+    setState((prevState) => ({
+      ...prevState,
+      showLogin: false,
+      showRegister: false,
+    }));
   };
 
   const handleShowLogin = () => {
-    setLogin((previousState) => {
-      return { ...previousState, showLogin: true };
-    });
+    setState((prevState) => ({ ...prevState, showLogin: true }));
   };
 
-  const handleEmail = (event) => {
-    setForm((previousState) => {
-      return { ...previousState, email: event.target.value };
-    });
+  const handleShowRegister = () => {
+    setState((prevState) => ({ ...prevState, showRegister: true }));
   };
 
-  const handlePassword = (event) => {
-    setForm((previousState) => {
-      return { ...previousState, password: event.target.value };
-    });
+  const handleOnChange = (e) => {
+    setState((prevState) => ({
+      ...prevState,
+      [e.target.id]: e.target.value,
+    }));
   };
 
-  const handleSubmit = (event) => {
-    event.preventDefault();
+  const handleRegister = (e) => {
+    e.preventDefault();
+
+    users.push({
+      id: Date.now(),
+      fullname: state.fullname,
+      email: state.email,
+      password: state.password,
+      phone: state.phone,
+      address: state.address,
+    });
+
+    localStorage.setItem("deweTourUsers", JSON.stringify(users));
+
+    alert("Register Successful");
+
+    handleSwitch();
+  };
+
+  const handleLogin = (e) => {
+    e.preventDefault();
 
     for (let user of users) {
-      if (form.email === user.email && form.password === user.password) {
-        setLogin((previousState) => {
-          return { ...previousState, showLogin: false, isLogin: true };
-        });
+      if (state.email === user.email && state.password === user.password) {
+        setState((prevState) => ({
+          ...prevState,
+          showLogin: false,
+          isLogin: true,
+        }));
         return alert("Login Successful");
       }
     }
 
-    alert("Sorry, please check your email and password is valid");
-  };
-
-  const handleSwitchRegister = () => {
-    setLogin((previousState) => {
-      return { ...previousState, showLogin: false };
-    });
+    alert("Sorry, your email or your password is invalid");
   };
 
   const handleLogout = () => {
-    setLogin((previousState) => {
-      return { ...previousState, isLogin: false };
-    });
+    setState((prevState) => ({ ...prevState, isLogin: false }));
+  };
+
+  const handleSwitch = () => {
+    if (state.showLogin) {
+      setState((prevState) => ({
+        ...prevState,
+        showLogin: false,
+        showRegister: true,
+      }));
+    } else {
+      setState((prevState) => ({
+        ...prevState,
+        showLogin: true,
+        showRegister: false,
+      }));
+    }
   };
 
   return (
@@ -72,7 +107,7 @@ export default function Header() {
           </a>
 
           <div className="auth">
-            {login.isLogin ? (
+            {state.isLogin ? (
               <>
                 <Image
                   src={Avatar}
@@ -94,17 +129,21 @@ export default function Header() {
                   </button>
                 </li>
                 <li className="nav-item col">
-                  <button type="button" className="btn btn-primary text-white">
+                  <button
+                    type="button"
+                    className="btn btn-primary text-white"
+                    onClick={handleShowRegister}
+                  >
                     Register
                   </button>
                 </li>
               </ul>
             )}
 
-            <Modal show={login.showLogin} onHide={handleClose} centered>
+            <Modal show={state.showLogin} onHide={handleClose} centered>
               <Modal.Body className="p-4">
                 <h4 className="text-center mt-2 mb-4 fw-bold fs-3">Login</h4>
-                <form onSubmit={handleSubmit}>
+                <form onSubmit={handleLogin}>
                   <label htmlFor="email" className="fw-bold mb-2">
                     Email
                   </label>
@@ -112,8 +151,8 @@ export default function Header() {
                     id="email"
                     type="email"
                     className="mb-4 form-control"
-                    value={form.email}
-                    onChange={handleEmail}
+                    value={state.email}
+                    onChange={handleOnChange}
                   />
                   <label htmlFor="password" className="fw-bold mb-2">
                     Password
@@ -122,8 +161,8 @@ export default function Header() {
                     id="password"
                     type="password"
                     className="mb-4 form-control"
-                    value={form.password}
-                    onChange={handlePassword}
+                    value={state.password}
+                    onChange={handleOnChange}
                   />
                   <button
                     type="submit"
@@ -136,7 +175,80 @@ export default function Header() {
                     style={{ fontSize: 14 }}
                   >
                     Don't have an account?{" "}
-                    <a href="#" onClick={handleSwitchRegister}>
+                    <a href="#" onClick={handleSwitch}>
+                      Click here
+                    </a>
+                  </div>
+                </form>
+              </Modal.Body>
+            </Modal>
+
+            <Modal show={state.showRegister} onHide={handleClose} centered>
+              <Modal.Body className="p-4">
+                <h4 className="text-center mt-2 mb-4 fw-bold fs-3">Register</h4>
+                <form onSubmit={handleRegister}>
+                  <label htmlFor="fullname" className="fw-bold mb-2">
+                    Full Name
+                  </label>
+                  <input
+                    id="fullname"
+                    type="text"
+                    className="mb-4 form-control"
+                    value={state.fullname}
+                    onChange={handleOnChange}
+                  />
+                  <label htmlFor="email" className="fw-bold mb-2">
+                    Email
+                  </label>
+                  <input
+                    id="email"
+                    type="email"
+                    className="mb-4 form-control"
+                    value={state.email}
+                    onChange={handleOnChange}
+                  />
+                  <label htmlFor="password" className="fw-bold mb-2">
+                    Password
+                  </label>
+                  <input
+                    id="password"
+                    type="password"
+                    className="mb-4 form-control"
+                    value={state.password}
+                    onChange={handleOnChange}
+                  />
+                  <label htmlFor="phone" className="fw-bold mb-2">
+                    Phone
+                  </label>
+                  <input
+                    id="phone"
+                    type="number"
+                    className="mb-4 form-control"
+                    value={state.phone}
+                    onChange={handleOnChange}
+                  />
+                  <label htmlFor="address" className="fw-bold mb-2">
+                    Address
+                  </label>
+                  <textarea
+                    id="address"
+                    type="number"
+                    className="mb-4 form-control"
+                    value={state.address}
+                    onChange={handleOnChange}
+                  />
+                  <button
+                    type="submit"
+                    className="btn btn-primary text-white w-100 fw-bold mb-3"
+                  >
+                    Register
+                  </button>
+                  <div
+                    className="text-muted text-center"
+                    style={{ fontSize: 14 }}
+                  >
+                    Have an account?{" "}
+                    <a href="#" onClick={handleSwitch}>
                       Click here
                     </a>
                   </div>
